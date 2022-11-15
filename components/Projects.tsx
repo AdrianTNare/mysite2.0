4e0412dbf55adf9b6ace1e9097aa04b3e632cc26
iamAdrian <adriantnare@icloud.com>
2022-11-15 12:37:39 +0200

    fix: next image styling

---
+++ components/Projects.tsx
@@ -0,0 +1,40 @@
+import { ProjectCard } from "./PorjectCard";
+
+export const Projects = () => {
+  return (
+    <div className="bg-mygreen-dark min-h-[85vh]">
+      <div className="max-w-5xl px-3 md:px-5 mx-auto pt-16 md:pt-16">
+        <h2 className="text-5xl text-mygreen-light font-bold text-center">
+          Projects
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 max-w-xl lg:max-w-3xl mx-auto py-20">
+          <ProjectCard
+            heading={"Personal Portfolio"}
+            body={
+              "A uniquw, responsive, and sleek website for presenting my work."
+            }
+            tools={["React", "DaisyUI"]}
+            codeSrc={"https://gitgub.com/Adriantnare"}
+            imgSrc={"https://placeimg.com/400/225/arch"}
+          />
+          <ProjectCard
+            heading={"Blog Frontend"}
+            body={
+              "Created a site allowing user to create, view, and comment on blog posts."
+            }
+            tools={["React", "DaisyUI"]}
+            codeSrc={""}
+            imgSrc={"https://placeimg.com/400/225/arch"}
+          />
+          <ProjectCard
+            heading={"Blog Backend"}
+            body={"Created a Java-based backend to support my Blog's frontend."}
+            tools={["Java", "Springboot"]}
+            codeSrc={""}
+            imgSrc={"https://placeimg.com/400/225/arch"}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
